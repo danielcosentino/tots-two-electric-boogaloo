49ccfa63e3ea6203de0f14d12e66fe236fa05e07
Cosentino <daniel.cosentino@knights.ucf.edu>
2022-05-01 02:23:31 -0400

    test frontend serving

--- index.js
+++ index.js
@@ -1632,7 +1632,11 @@ app.post("/api/generateSchedule", async (req, res) =>
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file. 
-app.get("*", (req, res) => 
+app.get("/", (req, res) => 
+{
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+app.get("/*", (req, res) => 
 {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
